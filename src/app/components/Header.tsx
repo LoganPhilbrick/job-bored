0b4839c8d15424a0ba0c2b692511133c749132fc
LoganPhilbrick <loganphilbrick99@gmail.com>
2025-06-08 19:53:31 -0400

    added basic header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="font-bold text-6xl my-24 ml-48">
+      <text className="bg-gradient-to-b from-sky-600 from-35% to-white to-10% text-transparent bg-clip-text pl-1">j</text>ob-bored
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="">Home</div>;
+  return <div className="flex items-center justify-center border border-sky-600 text-sky-600/70 rounded-2xl h-150 mx-48">jobs will appear here.</div>;
 }
